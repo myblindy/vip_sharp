ecf72c38a2fae52df89481afca7a0fd6149af29e
Marius Mitea <tweety.04.01@gmail.com>
2016-08-06 11:04:47 -0400

    added a parser stub for now

---
+++ vip_sharp/VIPParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Parsing;
+
+namespace vip_sharp
+{
+    public class VIPParser : Parser
+    {
+        public VIPParser(Grammar grammar) : base(grammar)
+        {
+        }
+    }
+}
